e206089288b2e8095883c20d61f4f244643c9bc9
Helen Du <helendu.614@gmail.com>
2018-01-07 22:49:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -11,5 +11,10 @@ def function is_prime(n)
             return false
         i = i  6
      return true
+     def prime?(n)
+        if n <= 1
+            false
+        else
+            (2..n/2).none? { |i| n % i == 0
    end
  end
