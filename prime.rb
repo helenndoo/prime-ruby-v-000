fe6045a1caa84734a8d0d2ba4e91bef27536944d
Helen Du <helendu.614@gmail.com>
2018-01-07 22:07:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  end
+  array = (2..integer).to_s
+  array.pop
+  if array.any? do |number|
+    integer % number == 0
+  end
+  return false
+else
+  return true
+end
+ende
