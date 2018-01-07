ff6894df15a26d5b9aff13bfd4a7af132e800885
Helen Du <helendu.614@gmail.com>
2018-01-07 22:08:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,6 @@
 # Add  code here!
 def prime?(n)
-  if n <= 1
+  if (n <= 1)
     return false
-  end
-  array = (2..integer).to_a
-  array.pop
-  if array.any? do |number|
-    integer % number == 0
-  end
-  return false
-else
-  return true
-end
-end
+  elsif (n <= 3)
+    return true
