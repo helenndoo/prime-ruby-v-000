4d6dec2a5fc1807ca7bf76f02daa75dff59a967d
Helen Du <helendu.614@gmail.com>
2018-01-07 22:09:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,17 @@
 # Add  code here!
 def prime?(n)
   if (n <= 1)
-    return false
+    false
   elsif (n <= 3)
-    return true
+    true
   elsif (num % 2 == 0 || num % 3 == 0)
     false
   else
     test_range = (2..num - 1).to_a
     if (test_range.any? {|test| num % test == 0})
-      return false
+      false
     else
-      return true
+      true
     end
   end
 end
